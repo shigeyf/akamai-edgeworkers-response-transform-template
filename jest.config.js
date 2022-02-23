35288a940dac6a8498ac4ecc94fcb3c545656887
Shige Fukushima <shigeyf@live.com>
2022-02-24 02:24:54 +0900

    Added new module maps for testing with Akamai EdgeWorkers local simulator codes.

--- jest.config.js
+++ jest.config.js
@@ -9,8 +9,11 @@ module.exports = {
     testEnvironment: "node",
     testMatch: ["<rootDir>/**/tests/**/*.test.(ts|tsx|js)"],
     moduleNameMapper: {
+        "^create-response$": "<rootDir>/tests/akamai-edgeworkers-localsim/create-response",
         "^streams$": "<rootDir>/tests/akamai-edgeworkers-localsim/streams",
-        "^log$": "<rootDir>/tests/akamai-edgeworkers-localsim/log"
+        "^text-encode-transform$": "<rootDir>/tests/akamai-edgeworkers-localsim/text-encode-transform",
+        "^http-request$": "<rootDir>/tests/akamai-edgeworkers-localsim/http-request",
+        "^log$": "<rootDir>/tests/akamai-edgeworkers-localsim/log",
     },
     verbose: true
 };
