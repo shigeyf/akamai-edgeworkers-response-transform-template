7847504d6926dd6f23e8260cd33de71baeabf784
Shige Fukushima <shigeyf@live.com>
2022-02-24 02:27:32 +0900

    Fixed a bug.

--- tests/akamai-edgeworkers-localsim/EW/interfaces/HeadersUI.ts
+++ tests/akamai-edgeworkers-localsim/EW/interfaces/HeadersUI.ts
@@ -1,5 +1,9 @@
 /**
- * akamai-edgeworkers-localsim/EW/interfaces/HeadersUI
+ * akamai-edgeworkers-localsim/EW/interfaces/Headers.ts
+ *
+ * Type definitions for non-npm package Akamai EdgeWorkers JavaScript API 1.0
+ */
+
 // This is what we receives from programmers' applications at the API.
 // For convenience reasons, the programmers can give a single header value string
 // instead of header value string array.
